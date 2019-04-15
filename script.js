c83f1bbee1b79cdec8de4f3835ae7a47b3fc0d23
Kristina <krdiamond@gmail.com>
2019-04-15 10:36:46 -0400

    added notation to describe JS

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
 $(document).ready(function(){
 
 // ----------------------------- MOBILE SLIDER ------------------------------ //
+// Initializing the slick slider funtionality for the featured image on mobile.
+// Provides an object with different customization options.
+
   $('.product-image-wrapper-mobile').slick({
     dots: true,
     slidesToShow: 1,
@@ -12,6 +15,9 @@ $(document).ready(function(){
   });
 
 // ------------------------------ ZOOM FUNCTION ----------------------------- //
+//User can click on the featured image to zoom in.
+//Image will take up the whole screen top to bottom, left to right.
+
   $('.product-image-wrapper-desktop').click(function(){
     $('.product-image-wrapper-desktop').clone().prependTo(".zoom");
   });
@@ -21,50 +27,55 @@ $(document).ready(function(){
   });
 
 // ----------------------------- COLOR SELECTOR ----------------------------- //
+//User can choose between the colors available.
+//Moves the check mark to the selected image
 
-var tick = $('.black .tick').html();
+  var tick = $('.black .tick').html();
 
-$('.beige').click(function(){
+  $('.beige').click(function(){
 
-  $('.black .tick').empty();
-  $('.beige .tick').append(tick);
+    $('.black .tick').empty();
+    $('.beige .tick').append(tick);
 
-});
+  });
 
-$('.black').click(function(){
-  $('.beige .tick').empty();
-  $('.black .tick').append(tick);
-});
+  $('.black').click(function(){
+    $('.beige .tick').empty();
+    $('.black .tick').append(tick);
+  });
 
 // ---------------------------- QUANTITY SELECTOR --------------------------- //
+//User can increase and decrease the quantity they would like to purcahse
+//Prevents a user from selecting a number lower than 1.
 
-$('.plus').click(function(){
-  var textNumber = $('.counter').text();
-  var integerCounter = parseInt(textNumber) + 1;
-  $('.counter').text(integerCounter)
-});
+  $('.plus').click(function(){
+    var textNumber = $('.counter').text();
+    var integerCounter = parseInt(textNumber) + 1;
+    $('.counter').text(integerCounter)
+  });
 
-$('.minus').click(function(){
-  var textNumber = $('.counter').text();
-  var integerCounter = parseInt(textNumber);
-  if (integerCounter > 1) {
-    var minusONE = integerCounter - 1;
-    $('.counter').text(minusONE);
-  }
-});
+  $('.minus').click(function(){
+    var textNumber = $('.counter').text();
+    var integerCounter = parseInt(textNumber);
+    if (integerCounter > 1) {
+      var minusONE = integerCounter - 1;
+      $('.counter').text(minusONE);
+    }
+  });
 
 // ----------------------------- SIZE SELECTOR ------------------------------ //
+//User can click on the size drop down box.
+//User can click a size and it will update the main text to show what has been selected.
 
-$('.size').click(function(){
-  $('.dropdown-content').toggle();
-});
+  $('.size').click(function(){
+    $('.dropdown-content').toggle();
+  });
 
-var size = $('.size-choice').text();
+  var size = $('.size-choice').text();
 
-$('.dropdown-content li').click(function(){
-  $('.size-choice').text($(this).text())
+  $('.dropdown-content li').click(function(){
+    $('.size-choice').text($(this).text())
+  });
 
-});
-
-
-});
+// -------------------------------------------------------------------------- //
+}); //end Document.ready()
